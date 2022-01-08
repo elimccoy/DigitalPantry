10aced3e316eef7801c556c69c40052cf2f99172
Elijah <elijmccoy01@gmail.com>
2022-01-08 14:25:59 -0800

    Main stack and Auth stack created in own dir

---
+++ DigitalPantry/navigation/Auth_Stack/AuthStack.js
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const aStack = createNativeStackNavigator();
+
+const AuthStack = () => {
+  return ( 
+    <aStack.Navigator>
+      <aStack.screen>
+        
+      </aStack.screen>
+    </aStack.Navigator>
+  );
+}
---
+++ DigitalPantry/navigation/Main_Stack/MainStack.js
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const mStack = createNativeStackNavigator();
+
+const AuthStack = () => {
+  return ( 
+    <mStack.Navigator>
+      <mStack.screen>
+        
+      </mStack.screen>
+    </mStack.Navigator>
+  );
+}
