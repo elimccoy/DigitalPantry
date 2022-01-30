ec1bff3d253c6b5c3305807989cf0f024295515c
Elijah <elijmccoy01@gmail.com>
2022-01-29 19:06:29 -0800

    Pantry item edit screen code cleanup. Styles changed to match pantry add screen

--- DigitalPantry/screens/Main_Stack/Pantry_Nav/PantryItemEditScreen.js
+++ DigitalPantry/screens/Main_Stack/Pantry_Nav/PantryItemEditScreen.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { TextInput } from 'react-native-paper';
+import { StatusBar } from 'expo-status-bar';
 import LoadingScreen from '../LoadingScreen';
 
 const PantryItemEditScreen = ({ route, navigation }) => {
@@ -11,6 +12,7 @@ const PantryItemEditScreen = ({ route, navigation }) => {
   const [unit, onChangeUnit] = useState('');
   const [amount, onChangeAmount] = useState('');
  
+  //Did mount:
   useEffect(() => {
     if(route.params !== undefined)
     {
@@ -18,7 +20,7 @@ const PantryItemEditScreen = ({ route, navigation }) => {
       setCurItem(passedItem);
       setIsLoaded(true);
     }
-  });
+  }, []);
 
   const handleConfirm = () => {
 
@@ -39,26 +41,27 @@ const PantryItemEditScreen = ({ route, navigation }) => {
     return(
       <View style={styles.container}>
         <View style={{ justifyContent: 'space-evenly' }}>
-          <Text style={styles.title}>Product Name: {curItem.key}</Text>
+          <Text style={styles.title}>Product Name: {curItem.name}</Text>
           <TextInput
             label="Name:"
             onChangeText={onChangeName}
-            defaultValue={name}
+            defaultValue={curItem.name}
           />
           <TextInput
             label="Unit:"
             onChangeText={onChangeUnit}
-            defaultValue={unit}
+            defaultValue={curItem.unit}
           />
           <TextInput
             label="Amount:"
             onChangeText={onChangeAmount}
-            defaultValue={amount}
+            defaultValue={curItem.amount}
           />
-          <Button
-            title='Confirm'
-            onPress={() => handleConfirm()}
-          />
+          <View style={{ flexDirection: 'row', padding: 10 }}>
+            <View style={{ flex: 1, padding: 10 }}>
+              <Button title='Done' onPress={() => {handleConfirm()}} />
+            </View>
+          </View>
         </View>
       </View>
     );
@@ -70,6 +73,7 @@ const PantryItemEditScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight,
     justifyContent: 'center'
   },
   title: {
