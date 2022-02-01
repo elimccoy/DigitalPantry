fee3f972accf5bdeff8a01280207e2f85c6ea855
anaacosta <ana.acosta.quiroz@gmail.com>
2022-02-01 05:48:01 -0800

    Added functionality to add items to list

--- DigitalPantry/components/SuggestedItem.js
+++ DigitalPantry/components/SuggestedItem.js
@@ -1,21 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 import {List, IconButton} from 'react-native-paper';
 import * as React from 'react';
+/**TO-DO
+ * Styling: change '+' color
+ * Display units
+ * */
 
 
-
-const SuggestedItem = ({item}) => {
+const SuggestedItem = ({item,addItem}) => { //takes in function addItem and also item
     
     const [itm, setItm] = React.useState(item);
-    const [amount, setAmount] = React.useState(itm[2]);
-    const [name, setName] = React.useState(itm[0]);
-    const [data, setData] = React.useState(itm[1]);
+    const [amount, setAmount] = React.useState(itm.amount);
+    const [name, setName] = React.useState(itm.name);
+    const [data, setData] = React.useState(itm.info);
+    const [key, setId] = React.useState(itm.key);
+
 
     return(
         <View >
 
             <List.Item title={name} description={data} left={() => <IconButton icon="plus-circle" 
-                size={20} onPress={() => console.log('Pressed')}/>} right={() => <Text> {amount} </Text>} />
+                size={20} onPress={() => addItem(itm)}/>} right={() => <Text> {amount} </Text>} />
  
         </View>
 
