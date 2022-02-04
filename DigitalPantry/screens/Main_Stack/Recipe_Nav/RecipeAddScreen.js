e63983a14035c0f5aa7c1a6ed9dfc9277d3a94e1
Thomas Galios <77800766+ThomasEmilio@users.noreply.github.com>
2022-02-04 10:12:45 -0800

    Change Requests addressed
    Formatting, unnecessary views removed, removed unnecessary comment, component changed to button, small style changes

--- DigitalPantry/screens/Main_Stack/Recipe_Nav/RecipeAddScreen.js
+++ DigitalPantry/screens/Main_Stack/Recipe_Nav/RecipeAddScreen.js
@@ -6,129 +6,115 @@ import UploadImage from './UploadImage';
 
 const RecipeAddScreen = ({ route, navigation }) => {
 
-    const { recipeName, onChangeName } = React.useState("Recipe Name");
-    const { ingList, onChangeIng } = React.useState("Ingredients");
-    const { recipeInfo, onChangeRecipe } = React.useState("Lorem ipsum dolor sit amet");
+  const { recipeName, onChangeName } = React.useState("Recipe Name");
+  const { ingList, onChangeIng } = React.useState("Ingredients");
+  const { recipeInfo, onChangeRecipe } = React.useState("Lorem ipsum dolor sit amet");
 
-    return (
+  return (
 
-        <ScrollView>
+    <ScrollView>
 
-            {/* REQUIRES npm install --save expo-image-picker */}
-            <UploadImage />
-            <View >
-                <TextInput
-                    style={styles.recipeName}
-                    onEndEditing={onChangeName}
-                    value={recipeName}
-                    placeholder="Recipe Name"
-                    inputAccessoryViewID="Done"
-                />
-            </View>
-            <View >
-                <TextInput
-                    style={styles.defaultInput}
-                    onEndEditing={onChangeIng}
-                    value={ingList}
-                    placeholder="Ingredients"
-                    inputAccessoryViewID="Done"
-                />
-            </View>
-            <View >
-                <TextInput
-                    style={styles.multilineInput}
-                    onChangeText={onChangeRecipe}
-                    value={recipeInfo}
-                    multiline={true}
-                    placeholder="Lorem ipsum dolor sit amet"
-                    inputAccessoryViewID="Done"
-                />
-            </View>
-            <View paddingHorizontal={12} alignItems={'left'}>
-                <TouchableOpacity
-                    style={styles.saveRecipeButton}
-                    onPress={() => navigation.navigate('RecipeScreen')}
-                >
-                    <Text style={{color: 'white', fontSize: 20, textAlign: 'center', marginTop: '15%'}}>
-                        Save
-                    </Text>
-                </TouchableOpacity>
-            </View>
+      <UploadImage />
+      <TextInput
+        style={styles.recipeName}
+        onEndEditing={onChangeName}
+        value={recipeName}
+        placeholder="Recipe Name"
+        inputAccessoryViewID="Done"
+      />
+      <TextInput
+        style={styles.multilineInput}
+        onChangeText={onChangeIng}
+        value={ingList}
+        multiline={true}
+        placeholder="Ingredients"
+        inputAccessoryViewID="Done"
+      />
+      <TextInput
+        style={styles.multilineInput}
+        onChangeText={onChangeRecipe}
+        value={recipeInfo}
+        multiline={true}
+        placeholder="Lorem ipsum dolor sit amet"
+        inputAccessoryViewID="Done"
+      />
+      <Button onPress={() => navigation.navigate('RecipeScreen')}>
+        Save
+      </Button>
 
+      <StatusBar style="dark" translucent={false} backgroundColor='white' />
 
-            <StatusBar style="dark" translucent={false} backgroundColor='white' />
+      <InputAccessoryView nativeID="Done">
+        <View style={styles.accessory}>
+          <TouchableOpacity
+            onPress={() => Keyboard.dismiss()}
+          >
+            <Text style={styles.doneButton}>
+              Done
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </InputAccessoryView>
 
-            <InputAccessoryView nativeID="Done">
-                <View style={styles.accessory}>
-                    <TouchableOpacity
-                        onPress={() => Keyboard.dismiss()}
-                    >
-                        <Text style={styles.doneButton}>
-                            Done
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </InputAccessoryView>
-
-        </ScrollView>
+    </ScrollView>
 
 
-    );
+  );
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight,
-        flexWrap: 'wrap',
-        flexDirection: 'column',
-        //justifyContent: 'center',
-        //alignItems: 'center',
-    },
-    recipeName: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 2,
-        fontSize: 36,
-    },
-    defaultInput: {
-        height: 36,
-        margin: 12,
-        borderWidth: 1,
-        padding: 2,
-        fontSize: 18,
-    },
-    multilineInput: {
-        margin: 12,
-        borderWidth: 1,
-        padding: 2,
-        fontSize: 18,
-    },
-    accessory: {
-        width: '100%',
-        height: 48,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        backgroundColor: '#F8F8F8',
-        paddingHorizontal: 8
-    },
-    doneButton: {
-        color: '#007AFF',
-        fontSize: 17
-    },
-    saveRecipeButton: {
-        height: 48,
-        width: '25%',
-        backgroundColor: '#007AFF',
-        borderRadius: 8,
-        padding: 2,
-        paddingHorizontal: 8,
-        alignItems: 'center',
-        
-    }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    //justifyContent: 'center',
+    //alignItems: 'center',
+  },
+  recipeName: {
+    height: 40,
+    //margin: 12,
+    //borderWidth: 1,
+    padding: 2,
+    fontSize: 36,
+  },
+  defaultInput: {
+    height: 36,
+    //margin: 12,
+    //borderWidth: 1,
+    padding: 2,
+    fontSize: 18,
+  },
+  multilineInput: {
+    //margin: 12,
+    //borderWidth: 1,
+    padding: 2,
+    fontSize: 18,
+  },
+  accessory: {
+    width: '100%',
+    height: 48,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#F8F8F8',
+    paddingHorizontal: 8
+  },
+  doneButton: {
+    color: '#007AFF',
+    fontSize: 17
+  },
+  saveRecipeButton: {
+    height: 48,
+    width: '25%',
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+    padding: 2,
+    paddingHorizontal: 8,
+    alignItems: 'center',
+
+  }
 
 });
 
