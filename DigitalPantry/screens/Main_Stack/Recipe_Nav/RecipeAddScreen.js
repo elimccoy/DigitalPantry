caa67abdd4afec86fb1e031a90e19c511ec689b5
Thomas Galios <77800766+ThomasEmilio@users.noreply.github.com>
2022-02-07 13:58:14 -0800

    Revert "Recipe add screen added to main"
    This reverts commit 835a8c462acd4309939d24262b3f81ad1f99a984.

--- DigitalPantry/screens/Main_Stack/Recipe_Nav/RecipeAddScreen.js
+++ DigitalPantry/screens/Main_Stack/Recipe_Nav/RecipeAddScreen.js
@@ -1,109 +1,19 @@
-import { InputAccessoryView, Keyboard, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, TouchableOpacityBase, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Avatar, Button, Card, Title, Paragraph, TextInput } from 'react-native-paper';
-import UploadImage from './UploadImage';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
 
 const RecipeAddScreen = ({ route, navigation }) => {
-  const { recipeName, onChangeName } = useState("Recipe Name");
-  const { ingList, onChangeIng } = useState("Ingredients");
-  const { recipeInfo, onChangeRecipe } = useState("Lorem ipsum dolor sit amet");
-
   return (
-    <KeyboardAwareScrollView>
-      <UploadImage />
-      <TextInput
-        style={styles.recipeName}
-        onEndEditing={onChangeName}
-        value={recipeName}
-        placeholder="Recipe Name"
-        inputAccessoryViewID="Done"
-      />
-      <TextInput
-        style={styles.multilineInput}
-        onChangeText={onChangeIng}
-        value={ingList}
-        multiline={true}
-        scrollEnabled={false}
-        placeholder="Ingredients"
-        inputAccessoryViewID="Done"
-      />
-      <TextInput
-        style={styles.multilineInput}
-        onChangeText={onChangeRecipe}
-        value={recipeInfo}
-        multiline={true}
-        scrollEnabled={false}
-        placeholder="Lorem ipsum dolor sit amet"
-        inputAccessoryViewID="Done"
-      />
-      <Button onPress={() => navigation.navigate('RecipeScreen')}>
-        Save
-      </Button>
-
-      <StatusBar style="dark" translucent={false} backgroundColor='white' />
-
-      <InputAccessoryView nativeID="Done">
-        <View style={styles.accessory}>
-          <TouchableOpacity
-            onPress={() => Keyboard.dismiss()}
-          >
-            <Text style={styles.doneButton}>
-              Done
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </InputAccessoryView>
-    </KeyboardAwareScrollView>
+    <View style={styles.container}>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // unused. TODO Remove
-  // container: {
-  //   flex: 1,
-  //   marginTop: StatusBar.currentHeight,
-  //   flexWrap: 'wrap',
-  //   flexDirection: 'column',
-  //   //justifyContent: 'center',
-  //   //alignItems: 'center',
-  // },
-  recipeName: {
-    height: 40,
-    //margin: 12,
-    //borderWidth: 1,
-    padding: 2,
-    fontSize: 36,
-  },
-  // unused. TODO Remove
-  // defaultInput: {
-  //   height: 36,
-  //   //margin: 12,
-  //   //borderWidth: 1,
-  //   padding: 2,
-  //   fontSize: 18,
-  // },
-  multilineInput: {
-    //margin: 12,
-    //borderWidth: 1,
-    padding: 2,
-    fontSize: 18,
-  },
-  accessory: {
-    width: '100%',
-    height: 48,
+  container: {
+    flex: 1,
+    flexWrap: 'wrap',
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    backgroundColor: '#F8F8F8',
-    paddingHorizontal: 8,
   },
-  doneButton: {
-    color: '#007AFF',
-    fontSize: 17,
-  },
-
 });
 
 export default RecipeAddScreen;
