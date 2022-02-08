af3c87bad09cc516a28e74d3708af026e60ce81e
anaacosta <ana.acosta.quiroz@gmail.com>
2022-02-07 16:29:45 -0800

    No meaningful changes

--- DigitalPantry/screens/Main_Stack/Shopping_Nav/ListAddScreen.js
+++ DigitalPantry/screens/Main_Stack/Shopping_Nav/ListAddScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, View, Button} from 'react-native';
 import * as React from 'react';
 import { TextInput } from 'react-native-paper';
 
@@ -7,89 +7,78 @@ import { TextInput } from 'react-native-paper';
  * change keys (hard coded for now)
  * Styling: make it more similar to pantry edit screen
  */
-const ListAddScreen = ({navigation}) => {
-    const [itm, setItm] = React.useState({});
-    const [itmName, setItmName] = React.useState("");
-    const [amt, setAmt] = React.useState("");
-    const [units, setUnits] = React.useState("");
-    const [key, setKey] = React.useState("")
-
-
-  const cancel = () =>{
-    navigation.navigate('ShoppingScreen'); 
-  }
+const ListAddScreen = ({ navigation }) => {
+  const [itm, setItm] = React.useState({});
+  const [itmName, setItmName] = React.useState("");
+  const [amt, setAmt] = React.useState("");
+  const [units, setUnits] = React.useState("");
+  const [key, setKey] = React.useState("")
 
   //adds item to list
-  const add = () =>{
-    let amtInt = parseInt(amt); 
+  const add = () => {
+    let amtInt = parseInt(amt);
     let k = (amtInt * 3).toString();
-    setKey(k)
-    var newItm = {key: k, amount: amt, name: itmName, data: units}; //make object
-   
-    setItm({key: key, amount: amt, name: itmName, data: units});
-    navigation.navigate('ShoppingScreen', {newItem:newItm}); //pass object to ShoppingScreen to be added
-  };
-  
-     
-  
+    setKey(k);
+    var newItm = { key: k, amount: amt, name: itmName, data: "idk" }; //make object
 
-  return(
-  
+    setItm({ key: key, amount: amt, name: itmName, data: units });
+    navigation.navigate('ShoppingScreen', { newItem: newItm }); //pass object to ShoppingScreen to be added
+  };
+
+
+  return (
+
     <View style={styles.container}>
 
-          <TextInput
-              style={styles.input}
-              label="Product Name"
-              value={itmName}
-              onChangeText={itmName => setItmName(itmName)}
-          />
+      <TextInput
+        style={styles.input}
+        label="Product Name"
+        value={itmName}
+        onChangeText={itmName => setItmName(itmName)}
+      />
 
-          <TextInput
-            style={styles.input}
-              label="Unit"
-              value={units}
-              onChangeText={units => setUnits(units)}
-          />
+      <TextInput
+        style={styles.input}
+        label="Unit"
+        value={units}
+        onChangeText={units => setUnits(units)}
+      />
 
-          <TextInput
-          style={styles.input}
-              label="Amount"
-              value={amt}
-              onChangeText={amt => setAmt(amt)}
-          />
+      <TextInput
+        style={styles.input}
+        label="Amount"
+        value={amt}
+        onChangeText={amt => setAmt(amt)}
+      />
 
-          <Button
-              title='Confirm'
-              onPress={() => add()}
-          />
-       
+      <Button
+        title='Confirm'
+        onPress={() => add()}
+      />
 
     </View>
   );
-
-
 
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center'
-    
-    },
-    title: {
-      fontSize: 30,
-      fontWeight: '700'
-    },
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center'
 
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '700'
+  },
+  input: {
+    height: 40,
+    margin: 17,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 
 export default ListAddScreen;
