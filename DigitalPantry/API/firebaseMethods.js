5e8c761f0c0e2093b86c83f65eaef1dbddcc2ff5
Caleb <choover11@gmail.com>
2022-02-14 10:45:54 -0800

    WIP; adding firebase functions

--- DigitalPantry/API/firebaseMethods.js
+++ DigitalPantry/API/firebaseMethods.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { collection, getDocs, query, where, addDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase';
 
@@ -10,3 +10,29 @@ export async function loggingOut() {
     // An error happened.
   });
 };
+
+const RecipesRef = collection(db, 'Recipes')
+
+// Following recipes are agnostic to the current logged in user and the userId should be passed into these functions
+
+// adds recipe to firestore database
+export async function fetchSavedRecipes(userId) {
+  try {
+    return getDocs(query(RecipesRef, where('userId', '==', userId)));
+  } catch (e) {
+    console.error('Error fetching recipes', e);
+  }
+}
+
+export async function saveRecipe(userId, recipe) {
+  try {
+    const ref = await addDoc(RecipesRef, {
+      userId: userId,
+      name: recipe.name,
+      ingredients: recipe.ingredients,
+
+    })
+  } catch (e) {
+    console.error('Error saving recipes', e);
+  }
+}
