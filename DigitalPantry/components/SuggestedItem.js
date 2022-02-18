ef5d7de920bec2915e48e4b5fd846b610c073b18
anaacosta <ana.acosta.quiroz@gmail.com>
2022-02-18 05:37:20 -0800

    consistent with pantry, adding to list still works the same

--- DigitalPantry/components/SuggestedItem.js
+++ DigitalPantry/components/SuggestedItem.js
@@ -1,28 +1,77 @@
-import { Text } from 'react-native';
-import {List, IconButton} from 'react-native-paper';
+import { Text, StyleSheet, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { List, IconButton, Badge } from 'react-native-paper';
 import * as React from 'react';
-/**TO-DO
- * Styling: change '+' color
- * Display units
- * */
+
+
+
+//onPress={() => addItem(item)
 
 const SuggestedItem = ({ item, addItem }) => { //takes in function addItem and also item
+
   return (
-    <List.Item
-      title={item.name}
-      description={item.data}
-      left={() => (
+    <View style={styles.container}>
+
+      <ImageBackground
+        source={{ uri: 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg' }}
+        resizeMode="cover"
+        style={styles.backgroundImgStyle}
+        imageStyle={styles.backgroundImgStyleImageStyles}>
+
         <IconButton
           icon="plus-circle"
-          size={20}
+          size={23}
           onPress={() => addItem(item)}
+          style={styles.addButton}
         />
-      )}
-      right={() => (
-        <Text>{item.amount}</Text>
-      )}
-    />
+
+        <Badge style={styles.nameBadge}>{item.name}</Badge>
+        <Badge style={styles.statusBadge}>{item.amount + ((item.info === undefined) ? "" : (" " + item.info))}</Badge>
+      </ImageBackground>
+    </View>
   );
 };
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    elevation: 3,
+    shadowColor: '#52006A',
+  },
+  backgroundImgStyle: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+  },
+  backgroundImgStyleImageStyles: {
+    borderRadius: 3,
+  },
+  statusBadge: {
+    position: 'absolute',
+    bottom: 7,
+    left: 7,
+    maxWidth: "70%",
+  },
+  nameBadge: {
+    position: 'absolute',
+    top: 7,
+    right: 7,
+    maxWidth: "70%",
+  },
+  addButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  },
+});
+
 
 export default SuggestedItem;
