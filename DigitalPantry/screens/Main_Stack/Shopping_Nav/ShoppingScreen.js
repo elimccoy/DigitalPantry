e1b4c7ad585d25dd0554b6c1b8d4b3b2a437bee5
anaacosta <ana.acosta.quiroz@gmail.com>
2022-02-18 05:45:21 -0800

    Consistent w/pantry Added CAB (not functional yet)

--- DigitalPantry/screens/Main_Stack/Shopping_Nav/ShoppingScreen.js
+++ DigitalPantry/screens/Main_Stack/Shopping_Nav/ShoppingScreen.js
@@ -1,16 +1,20 @@
-import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView, Dimensions, TouchableOpacity, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
-import { List, FAB } from 'react-native-paper';
+import { List, FAB, useTheme, Checkbox } from 'react-native-paper';
 import ShoppingListItem from '../../../components/ShoppingListItem.js';
 import SuggestedItem from '../../../components/SuggestedItem.js';
+import LoadingScreen from '../LoadingScreen.js';
+import ContextualActionBar from './ContextualActionBar';
+import { useNavigation } from '@react-navigation/native';
+import CustomNavigationBar from './CustomNavigationBar.js';
 
 /*TO-DO:
--implement delete all checked items function from the FAB
--display units for each item
+-implement select & delete all selected items 
+-Styling: fix scrolling
 */
 
 const list = [{ key: '1', name: 'Oranges', info: 'orange data', amount: '6' }, { key: '2', name: 'Apples', info: 'apple data', amount: '11' }, { key: '3', name: 'Broccoli', info: 'broccoli data', amount: '1' }];
-
+const numColumns = 2;
 const suggested = [{ key: '4', name: 'Onions', info: 'onion data', amount: '3' }, { key: '5', name: 'Cheese', info: 'cheese data', amount: '2' }, { key: '6', name: 'Milk', info: 'milk data', amount: '3' }];
 
 const ShoppingScreen = ({ route, navigation }) => {
@@ -19,9 +23,12 @@ const ShoppingScreen = ({ route, navigation }) => {
   const [showFab, setShowFab] = useState(true);
   const [expanded, setExpanded] = useState(true);
   const [state, setState] = useState({ open: false });
+  const [cabIsOpen, setCabIsOpen] = useState(false);
   const onStateChange = ({ open }) => setState({ open });
   const { open } = state;
   const handlePress = () => setExpanded(!expanded);
+
+
 
   useEffect(() => {
     if (route.params !== undefined) { //check if data from route is already in list, if not, add it
@@ -52,7 +59,35 @@ const ShoppingScreen = ({ route, navigation }) => {
       }
       setList(newList);
     };
+    
   }, [route.params]); //everytime parameters change
+
+ 
+  useEffect(() => {
+    if (cabIsOpen === true) {
+      //render cab
+      //console.log("open cab effect: " + cabIsOpen);
+      navigation.setOptions({
+        header: (props) => (<ContextualActionBar {...props} />)
+      });
+    } else {
+      navigation.setOptions({ header: (props) => <CustomNavigationBar {...props} /> });
+    }
+
+  }, [cabIsOpen]);
+
+  const openHeader = () => {
+    setCabIsOpen(true);
+  }
+
+  const closeHeader = () => {
+    setCabIsOpen(false);
+    //unselect items
+  }
+  const deleteSelected = () => {
+
+  }
+
 
   const manualAdd = () => { //navigate to ListAddScreen to get user's input to add custom item
     navigation.navigate('ListAddScreen');
@@ -78,33 +113,60 @@ const ShoppingScreen = ({ route, navigation }) => {
     //remove item from suggested list
     const filteredArr = suggItems.filter((itm) => (itm.key !== item.key));
     setSugg(filteredArr);
+
+    console.log(filteredArr);
   }
+
+  //Handle long press of item.
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        onLongPress={() => { navigation.navigate('ListEditDeleteScreen', { itemToEdit: item }) }}
+        onPress={() => { handlePress() }}
+        key={item.key}
+      >
+        <ShoppingListItem item={item} key={item.key} />
+      </TouchableOpacity>
+    );
+  };
+
+  //Handle long press of suggested item.
+  const renderSuggestedItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        key={item.key}
+      >
+        <SuggestedItem item={item} key={item.key} addItem={addSugg} />
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container} >
-      <ScrollView style={styles.container}>
-        <List.Section title="Shopping List" titleStyle={styles.listTitle}>
-          {listItems.map((i) =>
-            <TouchableOpacity onLongPress={() => { navigation.navigate('ListEditDeleteScreen', { itemToEdit: i }) }} key={i.key}>
-              <ShoppingListItem item={i} key={i.key} />
-            </TouchableOpacity>)}
-        </List.Section>
 
-        <List.Section style={styles.listSection}>
-          <List.Accordion title="Suggested Items" titleStyle={styles.listTitle} expanded={expanded} onPress={handlePress}>
-            {suggItems.map((i) => (<SuggestedItem item={i} key={i.key} addItem={addSugg} />))}
-            <List.Item />
-          </List.Accordion>
-        </List.Section>
+      <FlatList
+        data={listItems}
+        style={styles.scollContainer}
+        renderItem={renderItem}
+        numColumns={numColumns}
+      />
 
-      </ScrollView>
+      <FlatList
+        data={suggItems}
+        style={styles.scollContainer}
+        renderItem={renderSuggestedItem}
+        numColumns={numColumns}
+      />
+
       <FAB.Group
         visible={showFab}
         open={open}
         icon={'pencil'}
         style={styles.fab}
         actions={[
-          { icon: 'minus', label: 'Delete all checked items', onPress: () => console.log("minus") },
+          { icon: 'checkbox-marked-circle', label: 'Select items', onPress: () => openHeader() },
           { icon: 'plus', label: 'Add items', onPress: () => manualAdd() },
         ]}
         onStateChange={onStateChange}
@@ -113,9 +175,54 @@ const ShoppingScreen = ({ route, navigation }) => {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    elevation: 3,
+    shadowColor: '#52006A',
+  },
+  scollContainer: {
+    flex: 1,
+  },
+  item: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5,
+    height: Dimensions.get('window').width / numColumns, // approximate a square
+    width: Dimensions.get('window').width / numColumns - 10,
+  },
+  backgroundImgStyle: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+  },
+  backgroundImgStyleImageStyles: {
+    borderRadius: 3,
+  },
+  statusBadge: {
+    position: 'absolute',
+    bottom: 7,
+    left: 7,
+    maxWidth: "70%",
+  },
+  nameBadge: {
+    position: 'absolute',
+    top: 7,
+    right: 7,
+    maxWidth: "70%",
+  },
+  check: {
+    position: 'absolute',
+    top: 7,
+    left: 7,
+    maxWidth: "20%",
+    color: "purple",
   },
   fab: {
     margin: 0,
