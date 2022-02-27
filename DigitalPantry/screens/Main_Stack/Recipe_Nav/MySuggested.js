0833e935ba04b755e9000f59562a863016a67aca
Elijah <elijmccoy01@gmail.com>
2022-02-26 22:14:19 -0800

    Added query bar on suggested recipe screen

--- DigitalPantry/screens/Main_Stack/Recipe_Nav/MySuggested.js
+++ DigitalPantry/screens/Main_Stack/Recipe_Nav/MySuggested.js
@@ -1,15 +1,16 @@
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, TextInputComponent } from 'react-native';
 import { useState, useEffect } from 'react';
+import { Searchbar } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import RecipeItem from '../../../components/RecipeItem'
 import LoadingScreen from '../LoadingScreen';
 import { v4 as uuid } from 'uuid';
 import * as recAPI from '../../../API/recipes';
 
-const NUMSUGGESTED = 2;
+const NUMSUGGESTED = 3;
 
 const MySuggested = ({navigation}) => {
-  
+
   /*
     Data breakdown:
     title
@@ -19,14 +20,15 @@ const MySuggested = ({navigation}) => {
   */
 
   //States:
+  const [query, onChangeQuery] = useState(""); //Search Query state
   const [recipes, setRecipes] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  
+
   //Get ingredients from redux.
   const ingredients = useSelector((state) => state.pantry.ingredients);
 
-  //Force API call on update of ingredients.
-  useEffect(() =>{
+  //Set rendered recipes from ingredients.
+  const setRecipesFromIngredients = () => {
     //Determine search params by using ingredients.
     let concatList = "";
     ingredients.forEach((ingredient) => {
@@ -81,6 +83,72 @@ const MySuggested = ({navigation}) => {
       //Flag screen load state.
       setIsLoaded(true);
     });
+  }
+
+  //Set rendered recipes from query.
+  const setRecipesFromQuery = () => {
+    //Determine search params by using query.
+    let concatList = "";
+    let temp = "";
+    for(let i = 0; i < query.length; i++) {
+      if(query[i] === " ") {
+        concatList += temp + ",";
+        temp = "";
+      } else {
+        temp += query[i];
+      }
+    }
+
+    let searchParams = {
+      ingredients: concatList,
+      number: 1,
+    }
+
+    //Call API.
+    recAPI.fetchRecipesByIngredients(searchParams).then((res) => {
+      console.log("API Called");
+
+      //Create objs for each recipe.
+      let resRecipes = [];
+      for(let i = 0; i < res.length; i++) {
+        let missingIngredients = "";
+        let ownedIngredients = "";
+
+        console.log(res[i].missedIngredients)
+
+        //Make map of measurements and amounts.
+
+        for(let j = 0; j < res[i].missedIngredients.length; j++) {
+          missingIngredients += (res[i].missedIngredients[j].name + "\n");
+        }
+
+        for(let k = 0; k < res[i].usedIngredients.length; k++) {
+          ownedIngredients += (res[i].usedIngredients[k].name + "\n");
+        }
+
+        let newRecipe = {
+          id: uuid(),
+          category: 'Test Category',
+          title: res[i].title,
+          posterUrl: {uri: res[i].image},
+          missingIngredients: missingIngredients,
+          ownedIngredients: ownedIngredients,
+          steps: res[i].instructions,
+          sourceURL: res[i].sourceURL,
+          id: res[i].id,
+        }
+        console.log(newRecipe);
+        resRecipes.push(newRecipe);
+      }
+
+      //Save recipes to state.
+      setRecipes(resRecipes);
+    });
+  }
+
+  //Force API call on update of ingredients.
+  useEffect(() =>{
+    setRecipesFromIngredients();
   }, [ingredients]);
 
   //Render item for each flat list item.
@@ -94,9 +162,29 @@ const MySuggested = ({navigation}) => {
     );
   }
 
+  useEffect(() => {
+    console.log(query);
+
+    //Check if query is clear and replace with ingreditents suggested.
+    if(query === "") {
+      setRecipesFromIngredients();
+      return;
+    } else {
+      setRecipesFromQuery();
+    }
+
+  }, [query]);
+
   if(isLoaded) {
     return (
       <View style={styles.container}>
+        <View>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={(res) => { onChangeQuery(res) }}
+            value={query}
+            style={styles.searchBar} />
+        </View>
         <FlatList
           data={recipes}
           style={styles.scollContainer}
@@ -114,6 +202,9 @@ const MySuggested = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  searchBar: {
+    margin: 10,
   },
 });
 
