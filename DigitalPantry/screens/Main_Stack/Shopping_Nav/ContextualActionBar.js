64a78e05be78cb8bde8677a9bb81c2c2026b076e
anaacosta <ana.acosta.quiroz@gmail.com>
2022-03-01 07:47:01 -0800

    Implemented functionality

--- DigitalPantry/screens/Main_Stack/Shopping_Nav/ContextualActionBar.js
+++ DigitalPantry/screens/Main_Stack/Shopping_Nav/ContextualActionBar.js
@@ -1,13 +1,31 @@
-import { Appbar } from 'react-native-paper';
-const ContextualActionBar = ({ navigation, back }) => {
+import { Appbar, Button } from 'react-native-paper';
+import { deleteSelectedItems, selectAllItems } from '../../../store/slices/shoppingList';
+import { useDispatch } from 'react-redux';
+import { StyleSheet } from 'react-native';
+
+const ContextualActionBar = ({ close }) => {
+
+  const dispatch = useDispatch();
   return (
 
-    <Appbar.Header style={{width: '100%'}}>
-        <Appbar.Action icon='close' onPress={() => {}} />
-        <Appbar.Content title='Select items to delete' />
-        <Appbar.Action icon='delete' onPress={() => {}} />
+    <Appbar.Header style={{ width: '100%' }}>
+      <Appbar.Action icon='close' onPress={() => { close() }} />
+      <Appbar.Content title='Select items' />
+      <Button icon="checkbox-multiple-marked-circle-outline" constentStyle = {styles.buttonContent} mode="contained" compact = {true} dark = {false} color = "white" onPress={() => { dispatch(selectAllItems()) }}>
+        All
+      </Button>
+      <Appbar.Action icon='delete' onPress={() => { dispatch(deleteSelectedItems()); close() }} />
     </Appbar.Header>
-    
+
   );
 }
+
+
+const styles = StyleSheet.create({
+  buttonContent: {
+    borderWidth:1,
+    borderColor: 'white',
+
+  },
+});
 export default ContextualActionBar;
